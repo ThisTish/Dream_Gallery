647be6e345cbb44ccaf83364df5d5fec09105942
Tish <tishthegoat@gmail.com>
2024-11-11 21:12:59 -0700

    name

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,7 @@
 
 import { v2 as cloudinary } from "cloudinary"
 import Gallery from '../components/Gallery'
+import Hero from "@/components/landing/Hero"
 
 
 
@@ -10,8 +11,9 @@ import Gallery from '../components/Gallery'
 export default async function Home() {
 
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
+    <div className="">
+      <main className="">
+        <Hero />
         <h3> gallery? </h3>
         <Gallery />
     </main>
